45408d04bb33150098e0e3efac1f9278f709fded
PopovVladimir368678 <113527397+PopovVladimir368678@users.noreply.github.com>
2022-12-27 15:42:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-
 import requests
+import urllib.request
+from PIL import Image
 
-# api openweathermap не работает без vpn
-city_name = input('Введите название города(in english): ')
-responce = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+ city_name +'&appid=fae13c51f9c994c78e0ee2faf31511e8')
-                 # запрашиваю(апи сайта,метод погодаж; название города; ключ)
-weather_data = responce.json()
-if weather_data['cod'] == 200:  # проверяю удачность запроса и при удачном исходе вывожу
-    print('Погода в', city_name)
-    print('Температура', int(weather_data['main']['temp']) - 273, 'градусов по Цельсию')
-    print('Влажность', weather_data['main']['humidity'], '%')
-    print('Давление', weather_data['main']['pressure'], 'гПа ')
-else:
-    print('Город не найден')
+response = requests.get('https://apod.nasa.gov/apod/astropix.html').text
+url = 'https://apod.nasa.gov/apod/'
+image = ''
+i = response.find('IMG SRC=')+9
+while response[i] != '"':
+    image += response[i]
+    i += 1   # беру ссылку на картинку из того "текста который мне вернули"
 
+image_url = url + image
+print(image_url)
+urllib.request.urlretrieve(image_url, "photo.jpg")  #  скачиваю фото с сайта в свою папку
+
+
+img = Image.open("photo.jpg")
+img.show()    # открываю
